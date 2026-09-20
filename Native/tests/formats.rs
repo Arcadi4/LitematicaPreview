58a959646ec535964f113adfdb853ccda1fbba8d
tanhHeng <tanh_heng@outlook.com>
2026-09-20 20:51:32 +0800

    test: remove obsolete C ABI coverage

--- Native/tests/formats.rs
+++ Native/tests/formats.rs
@@ -22,24 +22,3 @@ fn minimal_formats_preserve_known_blocks_and_structure_entities() {
         }
     }
 }
-
-#[test]
-fn abi_layout_matches_x64_csharp_and_errors_are_owned() {
-    assert_eq!(std::mem::size_of::<PreviewInfo>(), 56);
-    assert_eq!(std::mem::size_of::<PartView>(), 56);
-    assert_eq!(std::mem::size_of::<TextureView>(), 24);
-    let mut pack = std::ptr::null_mut();
-    let mut error = ErrorBuffer::default();
-    let invalid = b"not a resource pack";
-    unsafe {
-        assert_ne!(
-            lp_pack_open(invalid.as_ptr(), invalid.len(), &mut pack, &mut error),
-            0
-        );
-        assert!(pack.is_null());
-        assert!(!error.data.is_null());
-        lp_error_free(error.data, error.len);
-        lp_preview_free(std::ptr::null_mut());
-        lp_pack_free(std::ptr::null_mut());
-    }
-}
